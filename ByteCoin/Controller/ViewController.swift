85d16e51c3cc265f394eac24b714aa1014a005fb
Soham Kayal <sohamkayal@Sohams-Laptop.local>
2024-06-15 02:56:43 +0530

    Added functionality to picker
    Signed-off-by: Soham Kayal <sohamkayal@Sohams-Laptop.local>

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,11 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate {
+    let coinManager = CoinManager();
+    
+    // for determining the total number of columns for the picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    // for determining the total number of rows for the picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count;
+    }
+    
+    // returns rowtitle for each row
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row];
+    }
+    
+    // prints the value of the row when selcted
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // print(coinManager.currencyArray[row]);
+        coinManager.getCoinPrice(for: coinManager.currencyArray[row]);
+    }
+    
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self;
+        currencyPicker.delegate = self;
     }
 
 
